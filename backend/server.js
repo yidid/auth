c3df035dfa36a31902b0df7723061dcb77bc38df
yidid <yididiya07@gmail.com>
2023-08-14 13:19:36 +0300

    modifing server.js

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,20 @@ import express from 'express';
 import dotenv from 'dotenv';
 import userRoutes from './routes/userRoutes.js'
 import { notFound,errorHandler } from './middleware/errorMiddleware.js';
+import connectDB from './config/db.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
+connectDB();
+
 const app= express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use(cookieParser());
+
 
 app.use('/api/users', userRoutes);
 
