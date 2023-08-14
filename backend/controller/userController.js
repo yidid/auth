4bb5387f86a67742d1e95bdd66f82b0c7b75b9b0
yidid <yididiya07@gmail.com>
2023-08-14 13:17:34 +0300

    modifying controllrts and routes

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,18 +1,70 @@
-import asyncHandler from 'express-async-handler'
+import asyncHandler from 'express-async-handler';
+import User from '../models/userModel.js';
+import generateToken from '../utils/generateToken.js';
+
+
 const authUser = asyncHandler(async(req,res)=>{
-    res.status(200).json({message :'Auth user'})
+    const {email,password}=req.body;
+
+    const user= await User.findOne({email});
+
+    if(user && (await user.matchPassword(password))){
+        generateToken(res,user._id )
+        res.status(201).json({
+            _id:user._id,
+            name:user.name,
+            email:user.email
+        })
+    }else{
+        res.status(400);
+        throw new Error('Invalid email or password')
+
+    }
+   
 });
 
 const registerUser = asyncHandler(async(req,res)=>{
-    res.status(200).json({message :' Register user'})
+    const {name,email,password}=req.body;
+    const userExist= await User.findOne({email})
+
+    if(userExist){
+        res.status(400);
+        throw new Error('user already exists');
+    }
+
+    const user= await User.create({
+        name,
+        email,
+        password
+    })
+    if(user){
+        generateToken(res,user._id )
+        res.status(201).json({
+            _id:user._id,
+            name:user.name,
+            email:user.email
+        })
+    }else{
+        res.status(400);
+        throw new Error('Invalid user data')
+
+    }
+   
 });
 
 const logoutUser = asyncHandler(async(req,res)=>{
-    res.status(200).json({message :'Logout user'})
+    res.cookie('jwt','',{
+        httpOnlt:true,
+        expires: new Date(0)
+
+    })
+   
+    res.status(200).json({message :'User logged out'})
 });
 
 const getUserProfile = asyncHandler(async(req,res)=>{
-    res.status(200).json({message :'User profile'})
+    console.log(req.user)
+   
 });
 
 const updateUserProfile = asyncHandler(async(req,res)=>{
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,19 @@
 import express from "express";
 const router = express.Router();
 
-import { authUser } from "../controller/userController.js";
+import {  
+    authUser,
+    logoutUser,
+    registerUser,
+    getUserProfile,
+    updateUserProfile
+ } from "../controller/userController.js";
+ import { protect } from "../middleware/authmiddleware.js";
 
 
-router.post('auth' ,authUser);
+router.post('/' ,registerUser);
+router.post('/auth' ,authUser);
+router.post('/logout' ,logoutUser);
+router.route('/profile').get(protect,getUserProfile).put(protect,updateUserProfile);
 
 export default router;
