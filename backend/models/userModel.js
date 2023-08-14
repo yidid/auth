f211fbb5391a4c3ceafa7b520fc59a1f79ae33ca
yidid <yididiya07@gmail.com>
2023-08-14 13:16:25 +0300

    adding models and utils

---
+++ backend/models/userModel.js
@@ -0,0 +1,40 @@
+
+import mongoose from "mongoose";
+import bcrypt from 'bcrypt'
+const Schema = mongoose.Schema;
+// Create Schema
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+UserSchema.pre('save', async function(next){
+    if(!this.isModified('password')){
+        next();
+
+    }
+    const salt= await bcrypt.genSalt(10);
+    this.password= await bcrypt.hash(this.password,salt)
+})
+
+UserSchema.methods.matchPassword= async function (enteredPassword){
+    return await bcrypt.compare(enteredPassword,this.password);
+}
+
+
+const User = mongoose.model("users", UserSchema);
+ export default User;
